fd69bad5deb3d6f8f2fa8eef0f3c78aa01495185
CAPM26 <cperezm30@miumg.edu.gt>
2024-10-08 20:04:18 -0600

    subir la plantilla del proyecto a Github

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}
+      <footer className='py-10 flex justify-center items-center'>
+          Hecho con ❤️ por el grupo #1 de desarrollo web de 8vo ciclo UMG jutiapa 
+        </footer>
+      </body>
+    </html>
+  );
+}
